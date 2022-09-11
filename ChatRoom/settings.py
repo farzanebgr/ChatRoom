7422888c7b510fad8ae8f602f7a2d8479eeb5aab
farzanebgr <farzenehboogari@gmail.com>
2022-09-11 14:49:49 +0430

    add channels Library

--- ChatRoom/settings.py
+++ ChatRoom/settings.py
@@ -26,7 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # INTERNAL_APPS
-    'chat',
+    'chat.apps.ChatConfig',
+    # EXTERNAL_APPS
+    'channels',
 ]
 
 MIDDLEWARE = [
