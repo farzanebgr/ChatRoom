be7ba90383f748e2b22d994a4884e3d4e6556762
farzanebgr <farzenehboogari@gmail.com>
2022-09-11 12:02:06 +0430

    Create two models in the chat application

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Room Table in database...
+class Room(models.Model):
+    name = models.CharField(max_length=128)
+    online = models.ManyToManyField(to=User, blank=True)
+
+    # How many users are online in this chat room?
+    def get_online_count(self):
+        return self.online.count()
+
+    # Change the online count when the user enters the room.
+    def join(self,user):
+        self.online.add(user)
+        return self.save()
+
+    # Change the online count when the user leaves the room
+    def leave(self, user):
+        self.online.remove(user)
+        return self.save()
+
+    # Display the name of the chat room and the number of users
+    def __str__(self):
+        return f'{self.name} ({self.get_online_count()})'
+
+
+
+# Message Table in database...
+class Message(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    room = models.ForeignKey(to=Room, on_delete=models.CASCADE)
+    content = models.CharField(max_length=512)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    # Display the name of user and the content and the time of send content
+    def __str__(self):
+        return f'{self.user.username}: {self.content} [{self.timestamp}]'
+
